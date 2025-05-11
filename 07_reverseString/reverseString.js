5bb6e94eb649c9b76b063d4058455f2f087f280a
grdnjs <201180648+grdnjs@users.noreply.github.com>
2025-05-11 15:00:17 -0500

    07 pass

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
 
+    let strArr = str.split('');
+    let res = '';
+    for (let i = str.length-1; i >= 0; i--) {
+        res += strArr[i];
+    }
+    return res;
 };
 
 // Do not edit below this line
