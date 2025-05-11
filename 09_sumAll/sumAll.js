f32ea7aa1b48b6979ce05105747239591a8b6034
grdnjs <201180648+grdnjs@users.noreply.github.com>
2025-05-11 18:26:14 -0500

    09 pass

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(min,max) {
+    if (isInvalid(min) || isInvalid(max)) return 'ERROR'
+    if (max < min) [min, max] = [max, min];
+    let sum = min;
+    if (min == max) {
+        return sum;
+    }else {
+        return sum + sumAll(min + 1, max)
+    }
 };
+
+const isInvalid = function(n) {
+    if ((!Number.isInteger(n)) || (n < 0)) return true;
+    return false;
+}
+
+sumAll(2,4);
 
 // Do not edit below this line
 module.exports = sumAll;
