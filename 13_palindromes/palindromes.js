358ea6709860f5f13aab9895705f8d1c0df227d7
grdnjs <201180648+grdnjs@users.noreply.github.com>
2025-05-11 19:08:04 -0500

    13 pass

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let arr = str.toLowerCase().split('');
+    arr = arr.filter(c => c.match(/\w/));
+    
+    for (let i = 0; i <= (Math.floor(arr.length/2)); i++) {
+        let k = arr.length - 1 - i;
+        if (arr[i] != arr[k]) return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
