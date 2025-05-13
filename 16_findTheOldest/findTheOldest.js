8636b8015d88799f61aa9c24244581d3bf449ca2
grdnjs <201180648+grdnjs@users.noreply.github.com>
2025-05-12 21:33:04 -0500

    16 pass

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,4 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest = 0;
+    let oldestPerson = "";
+    for (let person of people) {
+        let death = person.yearOfDeath ? person.yearOfDeath : new Date().getUTCFullYear();
+        let birth = person.yearOfBirth;
+        let age = death-birth;
+        console.log(age);
+        if (age > oldest) {
+            oldest = age;
+            oldestPerson  = person;
+            console.log(`Oldest: ${oldestPerson}`)
+        }
+    }
+    return oldestPerson;
 
 };
 
